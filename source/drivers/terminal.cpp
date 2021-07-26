f59d786adf59721b4937d66717aa2c545aa501fe
ilobilo <ilia.chachanidze35@gmail.com>
2021-07-26 20:51:29 +0400

    Terminal fixes

--- source/drivers/terminal.cpp
+++ source/drivers/terminal.cpp
@@ -2,20 +2,23 @@
 #include "../kernel.hpp"
 #include "../stivale2.h"
 #include "../include/string.hpp"
+#include "terminal.hpp"
 
-uint16_t framebuffer_width;
-uint16_t framebuffer_height;
+uint16_t columns;
+uint16_t rows;
 
 char *term_colour = "\033[37;40m";
 
 void (*term_write)(const char *string, int length);
 
-void term_init(struct stivale2_struct_tag_terminal *term_str_tag, struct stivale2_struct_tag_framebuffer *frm)
+void term_init(struct stivale2_struct_tag_terminal *term_str_tag)
 {
     void *term_write_ptr = (void *)term_str_tag->term_write;
     term_write = (void (*)(const char *string, int length))term_write_ptr;
-    framebuffer_width = frm->framebuffer_width;
-    framebuffer_height = frm->framebuffer_height;
+    columns = term_str_tag->cols;
+    rows = term_str_tag->rows;
+
+    term_clear();
 }
 
 void term_print(const char *string)
@@ -38,13 +41,14 @@ void term_resetcolour()
 void term_clear(char *ansii_colour)
 {
     term_print("\033[H");
-    for (uint16_t i = 0; i < framebuffer_width; i++)
+    for (uint16_t i = 0; i < rows; i++)
     {
-        for (uint16_t i = 0; i < framebuffer_height; i++)
+        for (uint16_t i = 0; i < columns; i++)
         {
             term_print(ansii_colour);
             term_print(" ");
         }
+        
     }
     term_print("\033[H");
 }
@@ -52,9 +56,9 @@ void term_clear(char *ansii_colour)
 void term_clear()
 {
     term_print("\033[H");
-    for (uint16_t i = 0; i < framebuffer_width; i++)
+    for (uint16_t i = 0; i < columns; i++)
     {
-        for (uint16_t i = 0; i < framebuffer_height; i++)
+        for (uint16_t i = 0; i < rows; i++)
         {
             term_print(term_colour);
             term_print(" ");
