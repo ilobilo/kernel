08a8e953112650b756983f6513fed17cd2e8e705
ilobilo <68286835+ilobilo@users.noreply.github.com>
2021-08-02 15:57:01 +0400

    update

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,23 @@
 #include "include/string.hpp"
 #include "drivers/terminal.hpp"
 #include "include/io.hpp"
-#include "system/gdt.hpp"
+#include "system/gdt/gdt.hpp"
 #include "stivale2.h"
 #include "kernel.hpp"
 
-void main(struct stivale2_struct *stivale2_struct)//struct stivale2_struct_tag_smp *smp)
+void main(struct stivale2_struct *stivale2_struct)
 {
+    struct stivale2_struct_tag_smp *smp_tag = (stivale2_struct_tag_smp (*))stivale2_get_tag(stivale2_struct, STIVALE2_STRUCT_TAG_SMP_ID);
+
     term_init((stivale2_struct_tag_terminal (*))stivale2_get_tag(stivale2_struct, STIVALE2_STRUCT_TAG_TERMINAL_ID));
+
     term_center("Welcome to kernel project!");
-    term_check(true, "Starting terminal...");
+
+    char *cpus;
+    int_to_string(smp_tag->cpu_count, cpus);
+    term_print("CPU cores available: ");
+    term_print(cpus);
+    term_print("\n");
+
     term_check(GDT_init(), "Initializing Global Descriptot Table...");
 }
---
+++ source/system/gdt/gdt.cpp
@@ -0,0 +1,21 @@
+#include "gdt.hpp"
+
+__attribute__((aligned(0x1000)))
+GDT DefaultGDT = {
+    {0, 0, 0, 0x00, 0x00, 0},
+    {0xffff, 0, 0, 0x9a, 0x80, 0},
+    {0xffff, 0, 0, 0x92, 0x80, 0},
+    {0xffff, 0, 0, 0x9a, 0xcf, 0},
+    {0xffff, 0, 0, 0x92, 0xcf, 0},
+    {0, 0, 0, 0x9a, 0xa2, 0},
+    {0, 0, 0, 0x92, 0xa0, 0},
+};
+
+bool GDT_init()
+{
+    GDTDescriptor gdtDescriptor;
+    gdtDescriptor.Size = sizeof(GDT) - 1;
+    gdtDescriptor.Offset = (uint64_t)&DefaultGDT;
+    LoadGDT(&gdtDescriptor);
+    return true;
+}
