f2597e09443609f2dff5132c66a78099dde38b89
ilobilo <68286835+ilobilo@users.noreply.github.com>
2021-08-05 19:17:42 +0400

    Update kernel.cpp

--- source/kernel.cpp
+++ source/kernel.cpp
@@ -56,7 +56,7 @@ void* stivale2_get_tag(stivale2_struct *stivale, uint64_t id)
 
 extern "C" void _start(struct stivale2_struct *stivale2_struct)
 {
-    main(stivale2_struct);//(stivale2_struct_tag_smp (*))stivale2_get_tag(stivale2_struct, STIVALE2_STRUCT_TAG_SMP_ID));
+    main(stivale2_struct);
 
     for (;;);
 }
