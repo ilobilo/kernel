09538dee1b6d8b4a91c15daeebe60f847f6b98f3
ilobilo <ilia.chachanidze35@gmail.com>
2021-08-05 19:13:13 +0400

    stivale paging

--- source/kernel.cpp
+++ source/kernel.cpp
@@ -20,10 +20,15 @@ static struct stivale2_header_tag_smp smp_hdr_tag = {
     .flags = 0
 };
 
+static struct stivale2_tag paging_hdr_tag = {
+    .identifier = STIVALE2_HEADER_TAG_5LV_PAGING_ID,
+    .next = (uint64_t)&smp_hdr_tag
+};
+
 static struct stivale2_header_tag_framebuffer framebuffer_hdr_tag = {
     .tag = {
         .identifier = STIVALE2_HEADER_TAG_FRAMEBUFFER_ID,
-        .next = (uint64_t)&smp_hdr_tag
+        .next = (uint64_t)&paging_hdr_tag
     },
     .framebuffer_width  = 0,
     .framebuffer_height = 0,
@@ -56,7 +61,7 @@ void* stivale2_get_tag(stivale2_struct *stivale, uint64_t id)
 
 extern "C" void _start(struct stivale2_struct *stivale2_struct)
 {
-    main(stivale2_struct);//(stivale2_struct_tag_smp (*))stivale2_get_tag(stivale2_struct, STIVALE2_STRUCT_TAG_SMP_ID));
+    main(stivale2_struct);
 
     for (;;);
 }
