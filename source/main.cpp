81cc32c3ca0e72cfd205d44edf9b1a4f61abddfc
ilobilo <68286835+ilobilo@users.noreply.github.com>
2021-08-22 15:55:40 +0400

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -13,7 +13,7 @@ void main(struct stivale2_struct *stivale2_struct)
 
     term_init((stivale2_struct_tag_terminal (*))stivale2_get_tag(stivale2_struct, STIVALE2_STRUCT_TAG_TERMINAL_ID));
 
-    term_center("Welcome to kernel project!");
+    term_center("Welcome to kernel project");
 
     term_print("CPU cores available: ");
     term_printi(smp_tag->cpu_count);
