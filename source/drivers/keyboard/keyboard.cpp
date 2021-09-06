c1efc11ea252d96a3e5fb834b124bbd555586f90
ilobilo <ilia.chachanidze35@gmail.com>
2021-09-07 01:01:03 +0400

    New and better keyboard driver with key combo support

--- source/drivers/keyboard/keyboard.cpp
+++ source/drivers/keyboard/keyboard.cpp
@@ -5,127 +5,43 @@
 #include <drivers/serial/serial.hpp>
 #include <drivers/terminal/terminal.hpp>
 #include <drivers/keyboard/keyboard.hpp>
+#include <drivers/keyboard/kbscancodetable.hpp>
 
-bool isCapsLock = false;
-bool isShiftPressed = false;
+static kbd_mod_t kbd_mod;
 
+// Scancode to ascii
 char get_ascii_char(uint8_t key_code)
 {
-    if ((isShiftPressed == true && isCapsLock == false) || (isShiftPressed == false && isCapsLock == true))
+    if (!kbd_mod.shift && !kbd_mod.capslock)
     {
-        switch(key_code)
-        {
-            case KEY_A : return 'A';
-            case KEY_B : return 'B';
-            case KEY_C : return 'C';
-            case KEY_D : return 'D';
-            case KEY_E : return 'E';
-            case KEY_F : return 'F';
-            case KEY_G : return 'G';
-            case KEY_H : return 'H';
-            case KEY_I : return 'I';
-            case KEY_J : return 'J';
-            case KEY_K : return 'K';
-            case KEY_L : return 'L';
-            case KEY_M : return 'M';
-            case KEY_N : return 'N';
-            case KEY_O : return 'O';
-            case KEY_P : return 'P';
-            case KEY_Q : return 'Q';
-            case KEY_R : return 'R';
-            case KEY_S : return 'S';
-            case KEY_T : return 'T';
-            case KEY_U : return 'U';
-            case KEY_V : return 'V';
-            case KEY_W : return 'W';
-            case KEY_X : return 'X';
-            case KEY_Y : return 'Y';
-            case KEY_Z : return 'Z';
-            case KEY_1 : return '!';
-            case KEY_2 : return '@';
-            case KEY_3 : return '#';
-            case KEY_4 : return '$';
-            case KEY_5 : return '%';
-            case KEY_6 : return '^';
-            case KEY_7 : return '&';
-            case KEY_8 : return '*';
-            case KEY_9 : return '(';
-            case KEY_0 : return ')';
-            case KEY_MINUS : return '_';
-            case KEY_EQUAL : return '+';
-            case KEY_SQUARE_OPEN_BRACKET : return '{';
-            case KEY_SQUARE_CLOSE_BRACKET : return '}';
-            case KEY_SEMICOLON : return ':';
-            case KEY_BACKSLASH : return '|';
-            case KEY_COMMA : return '<';
-            case KEY_DOT : return '>';
-            case KEY_FORESLHASH : return '?';
-            case KEY_SPACE : return ' ';
-            case KEY_ENTER : return '\n';
-            default : return 0;
-        }
+        return kbdus[key_code];
     }
-    else
+    if (kbd_mod.shift && !kbd_mod.capslock)
     {
-        switch(key_code)
-        {
-            case KEY_A : return 'a';
-            case KEY_B : return 'b';
-            case KEY_C : return 'c';
-            case KEY_D : return 'd';
-            case KEY_E : return 'e';
-            case KEY_F : return 'f';
-            case KEY_G : return 'g';
-            case KEY_H : return 'h';
-            case KEY_I : return 'i';
-            case KEY_J : return 'j';
-            case KEY_K : return 'k';
-            case KEY_L : return 'l';
-            case KEY_M : return 'm';
-            case KEY_N : return 'n';
-            case KEY_O : return 'o';
-            case KEY_P : return 'p';
-            case KEY_Q : return 'q';
-            case KEY_R : return 'r';
-            case KEY_S : return 's';
-            case KEY_T : return 't';
-            case KEY_U : return 'u';
-            case KEY_V : return 'v';
-            case KEY_W : return 'w';
-            case KEY_X : return 'x';
-            case KEY_Y : return 'y';
-            case KEY_Z : return 'z';
-            case KEY_1 : return '1';
-            case KEY_2 : return '2';
-            case KEY_3 : return '3';
-            case KEY_4 : return '4';
-            case KEY_5 : return '5';
-            case KEY_6 : return '6';
-            case KEY_7 : return '7';
-            case KEY_8 : return '8';
-            case KEY_9 : return '9';
-            case KEY_0 : return '0';
-            case KEY_MINUS : return '-';
-            case KEY_EQUAL : return '=';
-            case KEY_SQUARE_OPEN_BRACKET : return '[';
-            case KEY_SQUARE_CLOSE_BRACKET : return ']';
-            case KEY_SEMICOLON : return ';';
-            case KEY_BACKSLASH : return '\\';
-            case KEY_COMMA : return ',';
-            case KEY_DOT : return '.';
-            case KEY_FORESLHASH : return '/';
-            case KEY_SPACE : return ' ';
-            case KEY_ENTER : return '\n';
-            default : return 0;
-        }
+        return kbdus_shft[key_code];
+    }
+    if (!kbd_mod.shift && kbd_mod.capslock)
+    {
+        return kbdus_caps[key_code];
+    }
+    if (kbd_mod.shift && kbd_mod.capslock)
+    {
+        return kbdus_capsshft[key_code];
     }
 }
 
+// Handle key combinations
+void handle_comb(char c)
+{
+    // Example: ctrl + alt + w
+    if (kbd_mod.ctrl && kbd_mod.alt && c == 'w') printf("It works!");
+}
+
+// Keyboard buffer
 char* buff;
-//char* curs;
-//int count = 0;
 char c[2] = "\0";
 
+// Clear keyboard buffer
 void clearbuff()
 {
     for (int i = 0; i < strlen(buff); i++)
@@ -134,55 +50,76 @@ void clearbuff()
     }
 }
 
+// Main keyboard handler
 static void Keyboard_Handler(struct interrupt_registers *)
 {
     uint8_t scancode = inb(0x60);
-    switch (scancode)
+
+    if (scancode & 0x80)
     {
-        case KEY_CAPS_LOCK:
-            if (isCapsLock == false)
-            {
-                isCapsLock = true;
-            }
-            else
-            {
-                isCapsLock = false;
-            }
-            break;
-        case KEY_L_SHIFT_P:
-        case KEY_R_SHIFT_P:
-            isShiftPressed = true;
-            break;
-        case KEY_L_SHIFT_R:
-        case KEY_R_SHIFT_R:
-            isShiftPressed = false;
-            break;
-        case KEY_ENTER:
-            printf("\n");
-            clearbuff();
-            break;
-        case KEY_BACKSPACE:
-            if (buff[0] != '\0')
-            {
-                buff[strlen(buff) - 1] = '\0';
-                printf("\b \b");
-            }
-            break;
-        case KEY_LEFT:
-            cursor_left(1);
-//            curs[count] = buff[strlen(buff) - (count + 1)];
-//            count++;
-            break;
-        case KEY_RIGHT:
-            cursor_right(1);
-//            curs[count] = '\0';
-//            count--;
-            break;
-        default:
-            c[0] = get_ascii_char(scancode);
-            printf(c);
-            strcat(buff, c);
-            break;
+        // Key released
+    }
+    else
+    {
+        switch (scancode)
+        {
+            case L_SHIFT_DOWN:
+            case R_SHIFT_DOWN:
+                kbd_mod.shift = 1;
+                break;
+            case L_SHIFT_UP:
+            case R_SHIFT_UP:
+                kbd_mod.shift = 0;
+                break;
+            case CTRL_DOWN:
+                kbd_mod.ctrl = 1;
+                break;
+            case CTRL_UP:
+                kbd_mod.ctrl = 0;
+                break;
+            case ALT_DOWN:
+                kbd_mod.alt = 1;
+                break;
+            case ALT_UP:
+                kbd_mod.alt = 0;
+                break;
+            case CAPSLOCK:
+                kbd_mod.capslock = (!kbd_mod.capslock) ? 1 : 0;
+                break;
+            case NUMLOCK:
+                kbd_mod.numlock = (!kbd_mod.numlock) ? 1 : 0;
+                break;
+            case SCROLLLOCK:
+                kbd_mod.scrolllock = (!kbd_mod.scrolllock) ? 1 : 0;
+                break;
+            default:
+                c[0] = get_ascii_char(scancode);
+                if (kbd_mod.alt || kbd_mod.ctrl)
+                {
+                    handle_comb(c[0]);
+                    goto end;
+                }
+                switch (c[0])
+                {
+                    case '\n':
+                        printf("\n");
+                        clearbuff();
+                        break;
+                    case '\b':
+                        if (buff[0] != '\0')
+                        {
+                            buff[strlen(buff) - 1] = '\0';
+                            printf("\b \b");
+                        }
+                        break;
+                    default:
+                        printf(c);
+                        strcat(buff, c);
+                        break;
+                }
+                end:
+                break;
+        }
     }
 }
 
