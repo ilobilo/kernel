8738e17bac8df9f4965fed1a53d9c7e55bae0be6
ilobilo <ilia.chachanidze35@gmail.com>
2021-09-25 20:26:37 +0400

    update

--- source/include/io.cpp
+++ source/include/io.cpp
@@ -2,18 +2,40 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-uint8_t inb(uint16_t port)
-{
-    uint8_t ret;
-    asm volatile ( "inb %1, %0"
-                   : "=a"(ret)
-                   : "Nd"(port) );
-    return ret;
-}
-
 void outb(uint16_t port, uint8_t val)
 {
     asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+}
+
+void outw(uint16_t port, uint16_t val)
+{
+    asm volatile("outw %w0, %w1" : : "a" (val), "Nd" (port));
+}
+
+void outl(uint16_t port, uint32_t val)
+{
+    asm volatile("outl %0, %w1" : : "a" (val), "Nd" (port));
+}
+
+uint8_t inb(uint16_t port)
+{
+    uint8_t data;
+    asm volatile("inb %w1, %b0" : "=a" (data) : "Nd" (port));
+    return data;
+}
+
+uint16_t inw(uint16_t port)
+{
+    uint16_t data;
+    asm volatile("inw %w1, %w0" : "=a" (data) : "Nd" (port));
+    return data;
+}
+
+uint32_t inl(uint16_t port)
+{
+    uint32_t data;
+    asm volatile("inl %w1, %0" : "=a" (data) : "Nd" (port));
+    return data;
 }
 
 void io_wait(void)
--- source/include/io.hpp
+++ source/include/io.hpp
@@ -1,10 +1,19 @@
 #pragma once
+
 #include <stdint.h>
 #include <stdbool.h>
 
+void outb(uint16_t port, uint8_t val);
+
+void outw(uint16_t port, uint16_t val);
+
+void outl(uint16_t port, uint32_t val);
+
 uint8_t inb(uint16_t port);
 
-void outb(uint16_t port, uint8_t val);
+uint16_t inw(uint16_t port);
+
+uint32_t inl(uint16_t port);
 
 void io_wait(void);
 
