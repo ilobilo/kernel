fa23ca101dd34968620227fccfdc2867943e068e
ilobilo <ilia.chachanidze35@gmail.com>
2021-10-17 20:04:11 +0400

    Heap update

--- source/lib/io.cpp
+++ source/lib/io.cpp
@@ -40,25 +40,17 @@ uint32_t inl(uint16_t port)
 
 void io_wait(void)
 {
-    asm volatile ( "outb %%al, $0x80" : : "a"(0) );
+    asm volatile ("outb %%al, $0x80" : : "a"(0));
 }
 
 bool are_interrupts_enabled(bool should_print)
 {
     unsigned long flags;
-    asm volatile ( "pushf\n\t"
-                   "pop %0"
-                   : "=g"(flags) );
+    asm volatile ("pushf\n\t" "pop %0" : "=g"(flags));
     if (should_print)
     {
-        if (flags & (1 << 9))
-        {
-            printf("Interrupts are enabled!");
-        }
-        else
-        {
-            printf("Interrupts are not enabled!");
-        }
+        if (flags & (1 << 9)) printf("Interrupts are enabled!\n");
+        else printf("Interrupts are not enabled!\n");
     }
     return flags & (1 << 9);
 }
