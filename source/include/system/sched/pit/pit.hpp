722975e7d58ab4367c70ded2747d6b43afae2ee4
ilobilo <ilia.chachanidze35@gmail.com>
2021-10-17 20:28:03 +0400

    PIT updates

--- source/include/system/sched/pit/pit.hpp
+++ source/include/system/sched/pit/pit.hpp
@@ -2,10 +2,14 @@
 
 #include <stdint.h>
 
+extern uint64_t pit_frequency;
+
 extern bool pit_initialised;
 
 void PIT_sleep(double sec);
 
 uint64_t get_tick();
 
-void PIT_init();
+void PIT_setfreq(uint64_t freq = pit_frequency);
+
+void PIT_init(uint64_t freq = pit_frequency);
