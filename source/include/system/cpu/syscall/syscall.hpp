d60477ab297635729cd6ce7d0156e72162077edb
ilobilo <ilia.chachanidze35@gmail.com>
2021-10-21 21:18:27 +0400

    Syscall updates

--- source/include/system/cpu/syscall/syscall.hpp
+++ source/include/system/cpu/syscall/syscall.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
 #include <system/cpu/idt/idt.hpp>
+#include <stdint.h>
 
-char *syscall_read(int length);
-void syscall_write(char *string);
-void syscall_err(char *string);
+#define S_RAX regs->rax
+#define S_ARG0_RDI regs->rdi
+#define S_ARG1_RSI regs->rsi
+#define S_ARG2_RDX regs->rdx
+#define S_ARG3_R10 regs->r10
+#define S_ARG4_R8 regs->r8
+#define S_ARG5_R9 regs->r9
+
+using syscall_t = void (*)(interrupt_registers *);
+
+char *s_read(int length, char *string);
+void s_write(char *string);
+void s_err(char *string);
 
 extern "C" void syscall_handler(interrupt_registers *regs);
--- source/system/cpu/syscall/syscall.cpp
+++ source/system/cpu/syscall/syscall.cpp
@@ -5,71 +5,61 @@
 #include <system/mm/heap/heap.hpp>
 #include <lib/string.hpp>
 
-char *syscall_read(int length)
+static void syscall_read(interrupt_registers *regs)
 {
-    char *string = (char*)malloc((length + 1) * sizeof(char));
+    char *str = "Read currently not working\n";
+    S_ARG1_RSI = (uint64_t)str;
+}
+
+static void syscall_write(interrupt_registers *regs)
+{
+    switch (S_ARG0_RDI)
+    {
+        case 0:
+        {
+            char *str = (char*)malloc(S_ARG2_RDX * sizeof(char));
+            memcpy(str, (void*)S_ARG1_RSI, S_ARG2_RDX);
+            str[S_ARG2_RDX + 1] = 0;
+            printf("%s", str);
+            free(str);
+            break;
+        }
+        case 1:
+            break;
+        case 2:
+            serial_err("%s", (char*)S_ARG1_RSI);
+            break;
+        default:
+            break;
+    }
+}
+
+uint64_t syscall_count = 2;
+syscall_t syscalls[] = {
+    syscall_read,
+    syscall_write,
+};
+
+void syscall_handler(interrupt_registers *regs)
+{
+    if (S_RAX >= ZERO && S_RAX < syscall_count) syscalls[S_RAX](regs);
+}
+
+char *s_read(int length, char *string)
+{
     char *ret;
     asm volatile ("int $0x80" : "=a" (ret) : "0"(0), "D"(1), "S"(string), "d"(length) : "rcx", "r11", "memory");
     return string;
 }
-void syscall_write(char *string)
+void s_write(char *string)
 {
     char *ret;
     int length = strlen(string);
     asm volatile ("int $0x80" : "=a" (ret) : "0"(1), "D"(0), "S"(string), "d"(length) : "rcx", "r11", "memory");
 }
-void syscall_err(char *string)
+void s_err(char *string)
 {
     char *ret;
     int length = strlen(string);
     asm volatile ("int $0x80" : "=a" (ret) : "0"(1), "D"(2), "S"(string), "d"(length) : "rcx", "r11", "memory");
-}
-
-void syscall_handler(interrupt_registers *regs)
-{
-    switch (regs->rax)
-    {
-        case 0:
-            switch (regs->rdi)
-            {
-                case 0:
-                    break;
-                case 1:
-                {
-                    char *str = "Read currently not working\n";
-                    regs->rsi = (uint64_t)str;
-                    //for (uint64_t i = 0; i < regs->rdx; i++)
-                    //{
-                    //    *((uint8_t*)(regs->rsi + i)) = getchar();
-                    //}
-                    break;
-                }
-                case 2:
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case 1:
-            switch (regs->rdi)
-            {
-                case 0:
-                {
-                    char *str = (char*)malloc(regs->rdx * sizeof(char));
-                    memcpy(str, (void*)regs->rsi, regs->rdx);
-                    str[regs->rdx + 1] = 0;
-                    printf("%s", str);
-                    free(str);
-                    break;
-                }
-                case 1:
-                    break;
-                case 2:
-                    serial_err("%s", (char*)regs->rsi);
-                    break;
-                default:
-                    break;
-            }
-            break;
-    }
 }
