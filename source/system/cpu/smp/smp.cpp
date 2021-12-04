7ef875689e3c07d236791aaa8c560053998c9147
ilia <ilia.chachanidze35@gmail.com>
2021-12-04 18:35:00 +0400

    CPU updates

--- source/system/cpu/smp/smp.cpp
+++ source/system/cpu/smp/smp.cpp
@@ -10,6 +10,7 @@
 #include <kernel/main.hpp>
 #include <lib/lock.hpp>
 #include <lib/cpu.hpp>
+#include <cpuid.h>
 
 using namespace kernel::drivers::display;
 using namespace kernel::system::mm;
@@ -31,12 +32,54 @@ static void cpu_init(stivale2_smp_info *cpu)
 
     vmm::switchPagemap(vmm::kernel_pagemap);
 
-    wrmsr(0xC0000101, (uintptr_t)cpu->extra_argument);
-
-    InitSSE();
+    set_kernel_gs((uintptr_t)cpu->extra_argument);
+    set_user_gs((uintptr_t)cpu->extra_argument);
 
     this_cpu->lapic_id = cpu->lapic_id;
     this_cpu->tss = &gdt::tss[this_cpu->lapic_id];
+
+    enableSSE();
+    enableSMEP();
+    enableSMAP();
+    enableUMIP();
+    
+    uint64_t cr4 = 0;
+    uint32_t a = 0, b = 0, c = 0, d = 0;
+    __get_cpuid(1, &a, &b, &c, &d);
+    if ((c & bit_XSAVE))
+    {
+        cr4 = read_cr(4);
+        cr4 |= (1 << 18);
+        write_cr(4, cr4);
+        
+        uint64_t xcr0 = 0;
+        xcr0 |= (1 << 0);
+        xcr0 |= (1 << 1);
+        
+        if ((c & bit_AVX)) xcr0 |= (1 << 2);
+        
+        if (__get_cpuid(7, &a, &b, &c, &d))
+        {
+            if ((b & bit_AVX512F))
+            {
+                xcr0 |= (1 << 5);
+                xcr0 |= (1 << 6);
+                xcr0 |= (1 << 7);
+            }
+        }
+        wrxcr(0, xcr0);
+        
+        this_cpu->fpu_storage_size = (size_t)c;
+        
+        this_cpu->fpu_save = xsave;
+        this_cpu->fpu_restore = xrstor;
+	}
+    else
+    {
+        this_cpu->fpu_storage_size = 512;
+        this_cpu->fpu_save = fxsave;
+        this_cpu->fpu_restore = fxrstor;
+	}
 
     serial::info("CPU %ld is up", this_cpu->lapic_id);
     this_cpu->up = true;
