31e301e1dfb878640d3a811d448f69fc7cda8a9e
ilia <68286835+ilobilo@users.noreply.github.com>
2021-12-17 17:24:59 +0400

    Recursive PCI scan

--- source/system/pci/pci.cpp
+++ source/system/pci/pci.cpp
@@ -6,7 +6,6 @@
 #include <system/acpi/acpi.hpp>
 #include <system/pci/pci.hpp>
 #include <lib/string.hpp>
-#include <lib/io.hpp>
 
 using namespace kernel::drivers::display;
 using namespace kernel::drivers::fs;
@@ -16,51 +15,7 @@ namespace kernel::system::pci {
 bool initialised = false;
 bool legacy = false;
 
-static uint64_t currbus, currdev, currfunc;
-
 vector<translatedpcidevice_t*> pcidevices;
-
-static void get_addr(uint8_t bus, uint8_t dev, uint8_t func, uint32_t offset)
-{
-    uint32_t address = (bus << 16) | (dev << 11) | (func << 8) | (offset & ~(3)) | 0x80000000;
-    outl(0xcf8, address);
-}
-
-uint8_t readb(uint8_t bus, uint8_t dev, uint8_t func, uint32_t offset)
-{
-    get_addr(bus, dev, func, offset);
-    return inb(0xcfc + (offset & 3));
-}
-
-uint16_t readw(uint8_t bus, uint8_t dev, uint8_t func, uint32_t offset)
-{
-    get_addr(bus, dev, func, offset);
-    return inw(0xcfc + (offset & 3));
-}
-
-uint32_t readl(uint8_t bus, uint8_t dev, uint8_t func, uint32_t offset)
-{
-    get_addr(bus, dev, func, offset);
-    return inl(0xcfc + (offset & 3));
-}
-
-void writew(uint8_t bus, uint8_t dev, uint8_t func, uint32_t offset, uint16_t value)
-{
-    get_addr(bus, dev, func, offset);
-    outw(0xcfc + (offset & 3), value);
-}
-
-void writeb(uint8_t bus, uint8_t dev, uint8_t func, uint32_t offset, uint8_t value)
-{
-    get_addr(bus, dev, func, offset);
-    outb(0xcfc + (offset & 3), value);
-}
-
-void writel(uint8_t bus, uint8_t dev, uint8_t func, uint32_t offset, uint32_t value)
-{
-    get_addr(bus, dev, func, offset);
-    outl(0xcfc + (offset & 3), value);
-}
 
 translatedpcidevice_t *search(uint8_t Class, uint8_t subclass, uint8_t progif, int skip)
 {
@@ -154,26 +109,7 @@ size_t count(uint8_t Class, uint8_t subclass, uint8_t progif)
     return num;
 }
 
-translatedpcidevice_t *translate(pcidevice_t* device)
-{
-    translatedpcidevice_t *pcidevice = new translatedpcidevice_t;
-
-    pcidevice->device = device;
-
-    pcidevice->vendorstr = getvendorname(device->vendorid);
-    pcidevice->devicestr = getdevicename(device->vendorid, device->deviceid);
-    pcidevice->progifstr = getprogifname(device->Class, device->subclass, device->progif);
-    pcidevice->subclassStr = getsubclassname(device->Class, device->subclass);
-    pcidevice->ClassStr = device_classes[device->Class];
-
-    pcidevice->bus = currbus;
-    pcidevice->dev = currdev;
-    pcidevice->func = currfunc;
-
-    return pcidevice;
-}
-
-void enumfunc(uint64_t devaddr, uint64_t func)
+void enumfunc(uint64_t devaddr, uint64_t func, uint64_t dev, uint64_t bus)
 {
     uint64_t offset = func << 12;
     uint64_t funcaddr = devaddr + offset;
@@ -181,9 +117,21 @@ void enumfunc(uint64_t devaddr, uint64_t func)
     pcidevice_t *pcidevice = reinterpret_cast<pcidevice_t*>(funcaddr);
     if (pcidevice->deviceid == 0 || pcidevice->deviceid == 0xFFFF) return;
 
-    currfunc = func;
+    translatedpcidevice_t *tpcidevice = new translatedpcidevice_t;
+    tpcidevice->device = pcidevice;
 
-    pcidevices.push_back(translate(pcidevice));
+    tpcidevice->bus = bus;
+    tpcidevice->dev = dev;
+    tpcidevice->func = func;
+
+    tpcidevice->vendorstr = getvendorname(tpcidevice->device->vendorid);
+    tpcidevice->devicestr = getdevicename(tpcidevice->device->vendorid, tpcidevice->device->deviceid);
+    tpcidevice->progifstr = getprogifname(tpcidevice->device->Class, tpcidevice->device->subclass, tpcidevice->device->progif);
+    tpcidevice->subclassStr = getsubclassname(tpcidevice->device->Class, tpcidevice->device->subclass);
+    tpcidevice->ClassStr = device_classes[tpcidevice->device->Class];
+
+    pcidevices.push_back(tpcidevice);
+
     serial::info("%.4X:%.4X %s %s",
         pcidevices.last()->device->vendorid,
         pcidevices.last()->device->deviceid,
@@ -191,7 +139,7 @@ void enumfunc(uint64_t devaddr, uint64_t func)
         pcidevices.last()->devicestr);
 }
 
-void enumdevice(uint64_t busaddr, uint64_t dev)
+void enumdevice(uint64_t busaddr, uint64_t dev, uint64_t bus)
 {
     uint64_t offset = dev << 15;
     uint64_t devaddr = busaddr + offset;
@@ -199,11 +147,9 @@ void enumdevice(uint64_t busaddr, uint64_t dev)
     pcidevice_t *pcidevice = reinterpret_cast<pcidevice_t*>(devaddr);
     if (pcidevice->deviceid == 0 || pcidevice->deviceid == 0xFFFF) return;
 
-    currdev = dev;
-
     for (uint64_t func = 0; func < 8; func++)
     {
-        enumfunc(devaddr, func);
+        enumfunc(devaddr, func, dev, bus);
     }
 }
 
@@ -215,11 +161,64 @@ void enumbus(uint64_t baseaddr, uint64_t bus)
     pcidevice_t *pcidevice = reinterpret_cast<pcidevice_t*>(busaddr);
     if (pcidevice->deviceid == 0 || pcidevice->deviceid == 0xFFFF) return;
 
-    currbus = bus;
-
     for (uint64_t dev = 0; dev < 32; dev++)
     {
-        enumdevice(busaddr, dev);
+        enumdevice(busaddr, dev, bus);
+    }
+}
+
+void checkbus(int bus)
+{
+    for (size_t dev = 0; dev < 32; dev++)
+    {
+        for (size_t func = 0; func < 8; func++)
+        {
+            if (readl(bus, dev, func, 0) == 0xFFFFFFFF) continue;
+
+            translatedpcidevice_t *tpcidevice = new translatedpcidevice_t;
+            tpcidevice->device = new pcidevice_t;
+
+            tpcidevice->bus = bus;
+            tpcidevice->dev = dev;
+            tpcidevice->func = func;
+
+            uint32_t config_0 = tpcidevice->readl(0);
+            uint32_t config_4 = tpcidevice->readl(0x4);
+            uint32_t config_8 = tpcidevice->readl(0x8);
+            uint32_t config_c = tpcidevice->readl(0xC);
+
+            tpcidevice->device->vendorid = static_cast<uint16_t>(config_0);
+            tpcidevice->device->deviceid = static_cast<uint16_t>(config_0 >> 16);
+            tpcidevice->device->command = static_cast<uint16_t>(config_4);
+            tpcidevice->device->status = static_cast<uint16_t>(config_4 >> 16);
+            tpcidevice->device->revisionid = static_cast<uint8_t>(config_8);
+            tpcidevice->device->progif = static_cast<uint8_t>(config_8 >> 8);
+            tpcidevice->device->subclass = static_cast<uint8_t>(config_8 >> 16);
+            tpcidevice->device->Class = static_cast<uint8_t>(config_8 >> 24);
+            tpcidevice->device->cachelinesize = static_cast<uint8_t>(config_c);
+            tpcidevice->device->latencytimer = static_cast<uint8_t>(config_c >> 8);
+            tpcidevice->device->headertype = static_cast<uint8_t>(config_c >> 16);
+            tpcidevice->device->bist = static_cast<uint8_t>(config_c >> 24);
+
+            tpcidevice->vendorstr = getvendorname(tpcidevice->device->vendorid);
+            tpcidevice->devicestr = getdevicename(tpcidevice->device->vendorid, tpcidevice->device->deviceid);
+            tpcidevice->progifstr = getprogifname(tpcidevice->device->Class, tpcidevice->device->subclass, tpcidevice->device->progif);
+            tpcidevice->subclassStr = getsubclassname(tpcidevice->device->Class, tpcidevice->device->subclass);
+            tpcidevice->ClassStr = device_classes[tpcidevice->device->Class];
+
+            pcidevices.push_back(tpcidevice);
+
+            serial::info("%.4X:%.4X %s %s",
+                pcidevices.last()->device->vendorid,
+                pcidevices.last()->device->deviceid,
+                pcidevices.last()->vendorstr,
+                pcidevices.last()->devicestr);
+
+            if (tpcidevice->device->Class == 0x06 && tpcidevice->device->subclass == 0x04)
+            {
+                checkbus(reinterpret_cast<pciheader1*>(tpcidevice->device)->secBus);
+            }
+        }
     }
 }
 
@@ -234,8 +233,7 @@ void init()
     }
     if (!acpi::mcfghdr)
     {
-        serial::err("MCFG was not found");
-        serial::warn("This might take some time");
+        serial::warn("MCFG was not found!");
         legacy = true;
     }
 
@@ -254,46 +252,13 @@ void init()
     }
     else
     {
-        for (int bus = 0; bus < 256; bus++)
+        if ((static_cast<uint8_t>(readl(0, 0, 0, 0xC) >> 16) & 0x80) == 0) checkbus(0);
+        else
         {
-            for (int dev = 0; dev < 32; dev++)
+            for (size_t func = 0; func < 8; func++)
             {
-                for (int func = 0; func < 8; func++)
-                {
-                    uint32_t config_0 = readl(bus, dev, func, 0);
-                    if (config_0 == 0xFFFFFFFF) continue;
-
-                    uint32_t config_4 = readl(bus, dev, func, 0x4);
-                    uint32_t config_8 = readl(bus, dev, func, 0x8);
-                    uint32_t config_c = readl(bus, dev, func, 0xc);
-
-                    pcidevice_t *pcidevice = new pcidevice_t;
-
-                    pcidevice->vendorid = static_cast<uint16_t>(config_0);
-                    pcidevice->deviceid = static_cast<uint16_t>(config_0 >> 16);
-                    pcidevice->command = static_cast<uint16_t>(config_4);
-                    pcidevice->status = static_cast<uint16_t>(config_4 >> 16);
-                    pcidevice->revisionid = static_cast<uint8_t>(config_8);
-                    pcidevice->progif = static_cast<uint8_t>(config_8 >> 8);
-                    pcidevice->subclass = static_cast<uint8_t>(config_8 >> 16);
-                    pcidevice->Class = static_cast<uint8_t>(config_8 >> 24);
-                    pcidevice->cachelinesize = static_cast<uint8_t>(config_c);
-                    pcidevice->latencytimer = static_cast<uint8_t>(config_c >> 8);
-                    pcidevice->headertype = static_cast<uint8_t>(config_c >> 16);
-                    pcidevice->bist = static_cast<uint8_t>(config_c >> 24);
-
-                    currbus = bus;
-                    currdev = dev;
-                    currfunc = func;
-
-                    pcidevices.push_back(translate(pcidevice));
-
-                    serial::info("%.4X:%.4X %s %s",
-                        pcidevices.last()->device->vendorid,
-                        pcidevices.last()->device->deviceid,
-                        pcidevices.last()->vendorstr,
-                        pcidevices.last()->devicestr);
-                }
+                if (readl(0, 0, func, 0) != 0xFFFF) break;
+                checkbus(func);
             }
         }
     }
