6e09e9f6a8f763f352b41dd209db0fc951bbdf21
ilia <68286835+ilobilo@users.noreply.github.com>
2021-12-18 21:55:28 +0400

    AHCI read/write

---
+++ source/drivers/disk/ahci/ahci.cpp
@@ -0,0 +1,245 @@
+// Copyright (C) 2021  ilobilo
+
+#include <drivers/display/serial/serial.hpp>
+#include <drivers/disk/ahci/ahci.hpp>
+#include <system/mm/pmm/pmm.hpp>
+#include <lib/memory.hpp>
+
+using namespace kernel::drivers::display;
+using namespace kernel::system::mm;
+
+namespace kernel::drivers::disk::ahci {
+
+bool initialised = false;
+vector<AHCIDriver*> devices;
+
+AHCIPortType checkPortType(HBAPort *port)
+{
+    uint32_t statStat = port->SataStatus;
+    uint8_t intpowman = (statStat >> 8) & 0b111;
+    uint8_t devdetect = statStat & 0b111;
+
+    if (devdetect != HBA_PORT_DEV_PRESENT) return AHCIPortType::NONE;
+    if (intpowman != HBA_PORT_IPM_ACTIVE) return AHCIPortType::NONE;
+
+    switch (port->Signature)
+    {
+        case SATA_SIG_ATA:
+            return AHCIPortType::SATA;
+        case SATA_SIG_ATAPI:
+            return AHCIPortType::SATAPI;
+        case SATA_SIG_PM:
+            return AHCIPortType::PM;
+        case SATA_SIG_SEMB:
+            return AHCIPortType::SEMB;
+        default:
+            return AHCIPortType::NONE;
+    }
+}
+
+void AHCIDriver::probePorts()
+{
+    int portsImpl = ABAR->PortsImplemented;
+    for (uint64_t i = 0; i < 32; i++)
+    {
+        if (portsImpl & (1 << i))
+        {
+            AHCIPortType portType = checkPortType(&ABAR->Ports[i]);
+            if (portType == AHCIPortType::SATA || portType == AHCIPortType::SATAPI)
+            {
+                ports[portCount] = new AHCIPort;
+                ports[portCount]->portType = portType;
+                ports[portCount]->hbaport = &ABAR->Ports[i];
+                ports[portCount]->portNum = portCount;
+                portCount++;
+            }
+        }
+    }
+}
+
+void AHCIPort::configure()
+{
+    stopCMD();
+
+    void *newbase = pmm::alloc();
+    hbaport->CommandListBase = static_cast<uint32_t>(reinterpret_cast<uint64_t>(newbase));
+    hbaport->CommandListBaseUpper = static_cast<uint32_t>(reinterpret_cast<uint64_t>(newbase) >> 32);
+    memset(reinterpret_cast<void*>(hbaport->CommandListBase), 0, 1024);
+
+    void *fisBase = pmm::alloc();
+    hbaport->FISBaseAddress = static_cast<uint32_t>(reinterpret_cast<uint64_t>(fisBase));
+    hbaport->FISBaseAddressUpper = static_cast<uint32_t>(reinterpret_cast<uint64_t>(fisBase) >> 32);
+    memset(fisBase, 0, 256);
+
+    HBACommandHeader *commandHdr = reinterpret_cast<HBACommandHeader*>(hbaport->CommandListBase + (static_cast<uint64_t>(hbaport->CommandListBaseUpper) << 32));
+    for (int i = 0; i < 32; i++)
+    {
+        commandHdr[i].PRDTLength = 8;
+        void *cmdTableAddr = pmm::alloc();
+        uint64_t address = reinterpret_cast<uint64_t>(cmdTableAddr) + (i << 8);
+        commandHdr[i].CommandTableBaseAddress = static_cast<uint32_t>(address);
+        commandHdr[i].CommandTableBaseAddressUpper = static_cast<uint32_t>(static_cast<uint64_t>(address) >> 32);
+        memset(cmdTableAddr, 0, 256);
+    }
+
+    startCMD();
+}
+
+void AHCIPort::stopCMD()
+{
+    hbaport->CommandStatus &= ~HBA_PxCMD_ST;
+    hbaport->CommandStatus &= ~HBA_PxCMD_FRE;
+
+    while (true)
+    {
+        if (hbaport->CommandStatus & HBA_PxCMD_FR) continue;
+        if (hbaport->CommandStatus & HBA_PxCMD_CR) continue;
+        break;
+    }
+}
+
+void AHCIPort::startCMD()
+{
+    while (hbaport->CommandStatus & HBA_PxCMD_CR);
+    hbaport->CommandStatus |= HBA_PxCMD_FRE;
+    hbaport->CommandStatus |= HBA_PxCMD_ST;
+}
+
+size_t AHCIPort::findSlot()
+{
+    uint32_t slots = hbaport->SataActive | hbaport->CommandIssue;
+    for (uint8_t i = 0; i < 32; i++)
+    {
+        if ((slots & (1 << i)) == 0) return i;
+    }
+    return -1;
+}
+
+bool AHCIPort::rw(uint64_t sector, uint32_t sectorCount, uint16_t *buffer, bool write)
+{
+    hbaport->InterruptStatus = static_cast<uint32_t>(-1);
+    size_t spin = 0;
+    int slot = findSlot();
+    if (slot == -1) return false;
+
+    HBACommandHeader *cmdHdr = reinterpret_cast<HBACommandHeader*>(hbaport->CommandListBase | static_cast<uint64_t>(hbaport->CommandListBaseUpper) << 32);
+    cmdHdr += slot;
+    cmdHdr->CommandFISLength = sizeof(FIS_REG_H2D) / sizeof(uint32_t);
+    cmdHdr->Write = (write) ? 1 : 0;
+    cmdHdr->ClearBusy = 1;
+    cmdHdr->PRDTLength = static_cast<uint16_t>((sectorCount - 1) >> 4) + 1;
+
+    HBACommandTable *cmdtable = reinterpret_cast<HBACommandTable*>(cmdHdr->CommandTableBaseAddress | static_cast<uint64_t>(cmdHdr->CommandTableBaseAddressUpper) << 32);
+    memset(cmdtable, 0, sizeof(HBACommandTable) + (cmdHdr->PRDTLength - 1) * sizeof(HBAPRDTEntry));
+
+    size_t i = 0;
+    for (; i < cmdHdr->PRDTLength - 1; i++)
+    {
+        cmdtable->PRDTEntry[i].DataBaseAddress = static_cast<uint32_t>(reinterpret_cast<uint64_t>(buffer));
+        cmdtable->PRDTEntry[i].DataBaseAddressUpper = static_cast<uint32_t>(reinterpret_cast<uint64_t>(buffer) << 32);
+        cmdtable->PRDTEntry[i].ByteCount = 0x2000 - 1;
+        cmdtable->PRDTEntry[i].InterruptOnCompletion = 1;
+        buffer += 0x1000;
+        sectorCount -= 16;
+    }
+
+    cmdtable->PRDTEntry[i].DataBaseAddress = static_cast<uint32_t>(reinterpret_cast<uint64_t>(buffer));
+    cmdtable->PRDTEntry[i].DataBaseAddressUpper = static_cast<uint32_t>(reinterpret_cast<uint64_t>(buffer) << 32);
+    cmdtable->PRDTEntry[i].ByteCount = (sectorCount << 9) - 1;
+    cmdtable->PRDTEntry[i].InterruptOnCompletion = 1;
+
+    FIS_REG_H2D *cmdFIS = reinterpret_cast<FIS_REG_H2D*>(&cmdtable->CommandFIS);
+    cmdFIS->FISType = FIS_TYPE::FIS_TYPE_REG_H2D;
+    cmdFIS->CommandControl = 1;
+    cmdFIS->Command = (write) ? ATA_CMD_WRITE_DMA_EX : ATA_CMD_READ_DMA_EX;
+
+    cmdFIS->LBA0 = static_cast<uint8_t>(sector);
+    cmdFIS->LBA1 = static_cast<uint8_t>(sector >> 8);
+    cmdFIS->LBA2 = static_cast<uint8_t>(sector >> 16);
+    cmdFIS->LBA3 = static_cast<uint8_t>(sector >> 24);
+    cmdFIS->LBA4 = static_cast<uint8_t>(sector >> 32);
+    cmdFIS->LBA5 = static_cast<uint8_t>(sector >> 40);
+
+    cmdFIS->DeviceRegister = 1 << 6;
+
+    cmdFIS->CountLow = sectorCount & 0xFF;
+    cmdFIS->CountHigh = (sectorCount >> 8) & 0xFF;
+
+    while ((hbaport->TaskFileData & (ATA_DEV_BUSY | ATA_DEV_DRQ)) && spin < 1000000) spin++;
+    if (spin == 1000000)
+    {
+        serial::err("AHCI: Port is hung!");
+        return false;
+    }
+
+    hbaport->CommandIssue = 1 << slot;
+
+    while (true)
+    {
+        if ((hbaport->CommandIssue & (1 << slot)) == 0) break;
+        if (hbaport->InterruptStatus & HBA_PxIS_TFES)
+        {
+            serial::err("AHCI: %s error!", (write) ? "write" : "read");
+            return false;
+        }
+    }
+
+    if (hbaport->InterruptStatus & HBA_PxIS_TFES) return false;
+    while (hbaport->CommandIssue);
+
+    return true;
+}
+
+bool AHCIPort::read(uint64_t sector, uint32_t sectorCount, uint16_t *buffer)
+{
+    return rw(sector, sectorCount, buffer, false);
+}
+
+bool AHCIPort::write(uint64_t sector, uint32_t sectorCount, uint16_t *buffer)
+{
+    return rw(sector, sectorCount, buffer, true);
+}
+
+AHCIDriver::AHCIDriver(pci::pcidevice_t *pcidevice)
+{
+    this->pcidevice = pcidevice;
+    serial::info("Registering AHCI driver #%zu", devices.size() + 1);
+
+    ABAR = reinterpret_cast<HBAMemory*>(reinterpret_cast<pci::pciheader0*>(pcidevice)->BAR5);
+
+    probePorts();
+
+    for (size_t i = 0; i < portCount; i++)
+    {
+        AHCIPort *port = ports[i];
+        port->configure();
+    }
+}
+
+void init()
+{
+    serial::info("Initialising AHCI driver");
+
+    if (initialised)
+    {
+        serial::warn("AHCI driver has already been initialised!\n");
+        return;
+    }
+
+    size_t count = pci::count(0x01, 0x06, 0x01);
+    if (count == 0)
+    {
+        serial::err("No AHCI devices found!\n");
+        return;
+    }
+
+    devices.init(count);
+    for (size_t i = 0; i < count; i++)
+    {
+        devices.push_back(new AHCIDriver(pci::search(0x01, 0x06, 0x01, i)->device));
+    }
+
+    serial::newline();
+    initialised = true;
+}
+}
---
+++ source/drivers/disk/ahci/ahci.hpp
@@ -0,0 +1,268 @@
+// Copyright (C) 2021  ilobilo
+
+#pragma once
+
+#include <system/pci/pci.hpp>
+#include <stdint.h>
+
+using namespace kernel::system;
+
+namespace kernel::drivers::disk::ahci {
+
+#define ATA_DEV_BUSY 0x80
+#define ATA_DEV_DRQ 0x08
+
+#define ATA_CMD_READ_DMA_EX 0x25
+#define ATA_CMD_WRITE_DMA_EX 0x35
+
+#define HBA_PxIS_TFES (1 << 30)
+
+#define HBA_PORT_DEV_PRESENT 0x3
+#define HBA_PORT_IPM_ACTIVE 0x1
+#define SATA_SIG_ATAPI 0xEB140101
+#define SATA_SIG_ATA 0x00000101
+#define SATA_SIG_SEMB 0xC33C0101
+#define SATA_SIG_PM 0x96690101
+
+#define HBA_PxCMD_CR 0x8000
+#define HBA_PxCMD_FRE 0x0010
+#define HBA_PxCMD_ST 0x0001
+#define HBA_PxCMD_FR 0x4000
+
+#define HBA_PxCMD_CR 0x8000
+#define HBA_PxCMD_FRE 0x0010
+#define HBA_PxCMD_ST 0x0001
+#define HBA_PxCMD_FR 0x4000
+
+enum FIS_TYPE
+{
+    FIS_TYPE_REG_H2D = 0x27,
+    FIS_TYPE_REG_D2H = 0x34,
+    FIS_TYPE_DMA_ACT = 0x39,
+    FIS_TYPE_DMA_SETUP = 0x41,
+    FIS_TYPE_DATA = 0x46,
+    FIS_TYPE_BIST = 0x58,
+    FIS_TYPE_PIO_SETUP = 0x5F,
+    FIS_TYPE_DEV_BITS = 0xA1,
+};
+
+enum AHCIPortType
+{
+    NONE = 0,
+    SATA = 1,
+    SEMB = 2,
+    PM = 3,
+    SATAPI = 4
+};
+
+struct HBAPort
+{
+    uint32_t CommandListBase;
+    uint32_t CommandListBaseUpper;
+    uint32_t FISBaseAddress;
+    uint32_t FISBaseAddressUpper;
+    uint32_t InterruptStatus;
+    uint32_t InterruptEnable;
+    uint32_t CommandStatus;
+    uint32_t Reserved0;
+    uint32_t TaskFileData;
+    uint32_t Signature;
+    uint32_t SataStatus;
+    uint32_t SataControl;
+    uint32_t SataError;
+    uint32_t SataActive;
+    uint32_t CommandIssue;
+    uint32_t SataNotification;
+    uint32_t FISSwitchControl;
+    uint32_t Reserved1[11];
+    uint32_t Vendor[4];
+};
+
+struct HBAMemory
+{
+    uint32_t HostCapability;
+    uint32_t GlobalHostControl;
+    uint32_t InterruptStatus;
+    uint32_t PortsImplemented;
+    uint32_t Version;
+    uint32_t CCCControl;
+    uint32_t CCCPorts;
+    uint32_t EnclosureManagementLocation;
+    uint32_t EnclosureManagementControl;
+    uint32_t HostCapabilitiesExtended;
+    uint32_t BIOSHandoffControlStatus;
+    uint8_t Reserved0[0x74];
+    uint8_t Vendor[0x60];
+    HBAPort Ports[1];
+};
+
+struct HBACommandHeader
+{
+    uint8_t CommandFISLength : 5;
+    uint8_t ATAPI : 1;
+    uint8_t Write : 1;
+    uint8_t Prefetchable : 1;
+    uint8_t Reset : 1;
+    uint8_t BIST : 1;
+    uint8_t ClearBusy : 1;
+    uint8_t Reserved0 : 1;
+    uint8_t PortMultiplier : 4;
+    uint16_t PRDTLength;
+    uint32_t PRDBCount;
+    uint32_t CommandTableBaseAddress;
+    uint32_t CommandTableBaseAddressUpper;
+    uint32_t Reserved1[4];
+};
+
+struct HBAPRDTEntry
+{
+    uint32_t DataBaseAddress;
+    uint32_t DataBaseAddressUpper;
+    uint32_t Reserved0;
+    uint32_t ByteCount : 22;
+    uint32_t Reserved1 : 9;
+    uint32_t InterruptOnCompletion:1;
+};
+
+struct HBACommandTable
+{
+    uint8_t CommandFIS[64];
+    uint8_t ATAPICommand[16];
+    uint8_t Reserved[48];
+    HBAPRDTEntry PRDTEntry[];
+};
+
+struct FIS_REG_H2D
+{
+    uint8_t FISType;
+    uint8_t PortMultiplier : 4;
+    uint8_t Reserved0 : 3;
+    uint8_t CommandControl : 1;
+    uint8_t Command;
+    uint8_t FeatureLow;
+    uint8_t LBA0;
+    uint8_t LBA1;
+    uint8_t LBA2;
+    uint8_t DeviceRegister;
+    uint8_t LBA3;
+    uint8_t LBA4;
+    uint8_t LBA5;
+    uint8_t FeatureHigh;
+    uint8_t CountLow;
+    uint8_t CountHigh;
+    uint8_t ISOCommandCompletion;
+    uint8_t Control;
+    uint8_t Reserved1[4];
+};
+
+struct FIS_REG_D2H
+{
+    uint8_t FISType;
+    uint8_t PortMultiplier : 4;
+    uint8_t Reserved0 : 2;
+    uint8_t IntBit : 1;
+    uint8_t Reserved1 : 1;
+    uint8_t Status;
+    uint8_t Error;
+    uint8_t LBA0;
+    uint8_t LBA1;
+    uint8_t LBA2;
+    uint8_t DeviceRegister;
+    uint8_t LBA3;
+    uint8_t LBA4;
+    uint8_t LBA5;
+    uint8_t Reserved2;
+    uint8_t CountLow;
+    uint8_t CountHigh;
+    uint8_t Reserved3[2];
+    uint8_t Reserved4[4];
+};
+
+struct FIS_DATA
+{
+    uint8_t FISType;
+    uint8_t PortMultiplier : 4;
+    uint8_t Reserved0 : 4;
+    uint8_t Reserved1[2];
+    uint32_t Data[1];
+};
+
+struct FIS_PIO_SETUP
+{
+    uint8_t FISType;
+    uint8_t PortMultiplier : 4;
+    uint8_t Reserved0 : 1;
+    uint8_t DataDirection : 1;
+    uint8_t IntBit : 1;
+    uint8_t Reserved1 : 1;
+    uint8_t Status;
+    uint8_t Error;
+    uint8_t LBA0;
+    uint8_t LBA1;
+    uint8_t LBA2;
+    uint8_t DeviceRegister;
+    uint8_t LBA3;
+    uint8_t LBA4;
+    uint8_t LBA5;
+    uint8_t Reserved2;
+    uint8_t CountLow;
+    uint8_t CountHigh;
+    uint8_t Reserved3;
+    uint8_t EStatus;
+    uint16_t TransferCount;
+    uint8_t Reserved4[4];
+};
+
+struct FIS_DMA_SETUP
+{
+    uint8_t FISType;
+    uint8_t PortMultiplier : 4;
+    uint8_t Reserved0 : 1;
+    uint8_t DataDirection : 1;
+    uint8_t IntBit : 1;
+    uint8_t AutoActivate : 1;
+    uint8_t Reserved1[2];
+    uint64_t DMABufferID;
+    uint32_t Reserved2;
+    uint32_t DMABufferOffset;
+    uint32_t TransferCount;
+    uint32_t Reserved3;
+};
+
+class AHCIPort
+{
+    private:
+    bool rw(uint64_t sector, uint32_t sectorCount, uint16_t *buffer, bool write);
+
+    public:
+    void configure();
+    void stopCMD();
+    void startCMD();
+    size_t findSlot();
+
+    bool read(uint64_t sector, uint32_t sectorCount, uint16_t *buffer);
+    bool write(uint64_t sector, uint32_t sectorCount, uint16_t *buffer);
+
+    HBAPort *hbaport;
+    AHCIPortType portType;
+    uint8_t portNum;
+};
+
+class AHCIDriver
+{
+    public:
+    AHCIDriver(pci::pcidevice_t *pcidevice);
+
+    void probePorts();
+
+    pci::pcidevice_t *pcidevice;
+    HBAMemory *ABAR;
+    AHCIPort *ports[32];
+    uint8_t portCount;
+};
+
+extern bool initialised;
+extern vector<AHCIDriver*> devices;
+
+void init();
+}
