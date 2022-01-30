79f367e337e205c7f0fdaf8651e22c23a00fe89b
ilobilo <ilia.chachanidze35@gmail.com>
2022-01-30 14:41:13 +0400

    Fixed shutdown

--- source/apps/kshell.cpp
+++ source/apps/kshell.cpp
@@ -248,7 +248,7 @@ void parse(char *cmd, char *arg)
         case hash("shutdown"):
         case hash("poweroff"):
             acpi::shutdown();
-            pit::sleep(1);
+            timer::sleep(1);
             outw(0xB004, 0x2000);
             outw(0x604, 0x2000);
             outw(0x4004, 0x3400);
