4a00d554c311d391a366432154b41f2ad6255dc6
ilobilo <68286835+ilobilo@users.noreply.github.com>
2022-03-10 19:41:28 +0400

    RTL8169 update

--- source/drivers/net/rtl8169/rtl8169.hpp
+++ source/drivers/net/rtl8169/rtl8169.hpp
@@ -11,6 +11,52 @@ using namespace kernel::system;
 
 namespace kernel::drivers::net::rtl8169 {
 
+enum regs
+{
+    REG_ID_0 = 0x00,
+    REG_ID_1 = 0x01,
+    REG_ID_2 = 0x02,
+    REG_ID_3 = 0x03,
+    REG_ID_4 = 0x04,
+    REG_ID_5 = 0x05,
+    REG_TNPDS_1 = 0x20,
+    REG_TNPDS_2 = 0x24,
+    REG_CR = 0x37,
+    REG_TPPOLL = 0x38,
+    REG_IMR = 0x3C,
+    REG_ISR = 0x3E,
+    REG_TCR = 0x40,
+    REG_RCR = 0x44,
+    REG_9346CR = 0x50,
+    REG_CONFIG1 = 0x52,
+    REG_RMS = 0xDA,
+    REG_RDSAR_1 = 0xE4,
+    REG_RDSAR_2 = 0xE8,
+    REG_ETTHR = 0xEC
+};
+
+enum cmd
+{
+    CMD_TX_ENABLE = (1 << 2),
+    CMD_RX_ENABLE = (1 << 3),
+    CMD_RESET = (1 << 4)
+};
+
+enum intstat
+{
+    IST_RECEIVE_OK = (1 << 0),
+    IST_RECEIVE_ERR = (1 << 1),
+    IST_TRANSMIT_OK = (1 << 2),
+    IST_TRANSMIT_ERR = (1 << 3),
+    IST_RX_UNAVAIL = (1 << 4),
+    IST_LINK_CHANGE = (1 << 5),
+    IST_RX_FIFO_OVER = (1 << 6),
+    IST_TX_UNAVAIL = (1 << 7),
+    IST_SOFT_INT = (1 << 8),
+    IST_TIME_OUT = (1 << 14),
+    IST_SYSTEM_ERROR = (1 << 15),
+};
+
 #define RTL8169_OWN (1 << 31)
 #define RTL8169_EOR (1 << 30)
 #define RTL8169_FFR (1 << 29)
