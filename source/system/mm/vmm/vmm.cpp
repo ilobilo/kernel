95e4af561fde8985cbefbe07ac3ac3dd68b829f9
ilobilo <ilia.chachanidze35@gmail.com>
2022-03-23 22:10:20 +0400

    vmm: MapMemRange fix and 2mb pages support, memory usage reduced to 1.8mb

--- source/system/mm/vmm/vmm.cpp
+++ source/system/mm/vmm/vmm.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2021-2022  ilobilo
 
 #include <drivers/display/framebuffer/framebuffer.hpp>
+#include <drivers/display/terminal/terminal.hpp>
 #include <system/mm/pmm/pmm.hpp>
 #include <system/mm/vmm/vmm.hpp>
 #include <kernel/kernel.hpp>
@@ -33,7 +34,7 @@ static PTable *get_next_lvl(PTable *curr_lvl, size_t entry, bool allocate = true
     return ret;
 }
 
-PDEntry *Pagemap::virt2pte(uint64_t vaddr, bool allocate)
+PDEntry *Pagemap::virt2pte(uint64_t vaddr, bool allocate, bool hugepages)
 {
     size_t pml5_entry = (vaddr & (static_cast<uint64_t>(0x1FF) << 48)) >> 48;
     size_t pml4_entry = (vaddr & (static_cast<uint64_t>(0x1FF) << 39)) >> 39;
@@ -55,30 +56,32 @@ PDEntry *Pagemap::virt2pte(uint64_t vaddr, bool allocate)
     }
     pml3 = get_next_lvl(pml4, pml4_entry, allocate);
     pml2 = get_next_lvl(pml3, pml3_entry, allocate);
+    if (hugepages) return &pml2->entries[pml2_entry];
+
     pml1 = get_next_lvl(pml2, pml2_entry, allocate);
     return &pml1->entries[pml1_entry];
 }
 
-void Pagemap::mapMem(uint64_t vaddr, uint64_t paddr, uint64_t flags)
+void Pagemap::mapMem(uint64_t vaddr, uint64_t paddr, uint64_t flags, bool hugepages)
 {
     lockit(this->lock);
 
-    PDEntry *pml1_entry = this->virt2pte(vaddr);
-    if (pml1_entry == nullptr)
+    PDEntry *pml_entry = this->virt2pte(vaddr, true, hugepages);
+    if (pml_entry == nullptr)
     {
         error("VMM: Could not get page map entry!");
         return;
     }
 
-    pml1_entry->setAddr(paddr >> 12);
-    pml1_entry->setflags(flags, true);
+    pml_entry->setAddr(paddr >> 12);
+    pml_entry->setflags(flags | (hugepages ? LargerPages : 0), true);
 }
 
-void Pagemap::mapMemRange(uint64_t vaddr, uint64_t paddr, uint64_t pagecount, uint64_t flags)
+void Pagemap::mapMemRange(uint64_t vaddr, uint64_t paddr, uint64_t size, uint64_t flags, bool hugepages)
 {
-    for (size_t i = 0; i < pagecount; i += 0x1000)
+    for (size_t i = 0; i < size; i += (hugepages ? large_page_size : page_size))
     {
-        this->mapMem(vaddr + i, paddr + i, flags);
+        this->mapMem(vaddr + i, paddr + i, flags, hugepages);
     }
 }
 
@@ -102,56 +105,55 @@ void Pagemap::remapMem(uint64_t vaddr_old, uint64_t vaddr_new, uint64_t flags)
     this->mapMem(vaddr_new, paddr, flags);
 }
 
-void Pagemap::unmapMem(uint64_t vaddr)
+void Pagemap::unmapMem(uint64_t vaddr, bool hugepages)
 {
     lockit(this->lock);
 
-    PDEntry *pml1_entry = this->virt2pte(vaddr, false);
-    if (pml1_entry == nullptr)
+    PDEntry *pml_entry = this->virt2pte(vaddr, false, hugepages);
+    if (pml_entry == nullptr)
     {
         error("VMM: Could not get page map entry!");
-        this->lock.unlock();
         return;
     }
 
-    pml1_entry->value = 0;
+    pml_entry->value = 0;
     invlpg(vaddr);
 }
 
-void Pagemap::unmapMemRange(uint64_t vaddr, uint64_t pagecount)
+void Pagemap::unmapMemRange(uint64_t vaddr, uint64_t size, bool hugepages)
 {
-    for (size_t i = 0; i < pagecount; i += 0x1000)
+    for (size_t i = 0; i < size; i += (hugepages ? large_page_size : page_size))
     {
-        this->unmapMem(vaddr);
+        this->unmapMem(vaddr + i);
     }
 }
 
-void Pagemap::setflags(uint64_t vaddr, uint64_t flags, bool enabled)
+void Pagemap::setflags(uint64_t vaddr, uint64_t flags, bool hugepages, bool enabled)
 {
     lockit(this->lock);
 
-    PDEntry *pml1_entry = this->virt2pte(vaddr, false);
-    if (pml1_entry == nullptr)
+    PDEntry *pml_entry = this->virt2pte(vaddr, false);
+    if (pml_entry == nullptr)
     {
         error("VMM: Could not get page map entry!");
         this->lock.unlock();
         return;
     }
-    pml1_entry->setflags(flags, enabled);
+    pml_entry->setflags(flags | (hugepages ? LargerPages : 0), enabled);
 }
 
-bool Pagemap::getflags(uint64_t vaddr, uint64_t flags)
+bool Pagemap::getflags(uint64_t vaddr, uint64_t flags, bool hugepages)
 {
     lockit(this->lock);
 
-    PDEntry *pml1_entry = this->virt2pte(vaddr, false);
-    if (pml1_entry == nullptr)
+    PDEntry *pml_entry = this->virt2pte(vaddr, false);
+    if (pml_entry == nullptr)
     {
         error("VMM: Could not get page map entry!");
         this->lock.unlock();
         return false;
     };
-    return pml1_entry->getflags(flags);
+    return pml_entry->getflags(flags);
 }
 
 Pagemap *newPagemap()
@@ -172,27 +174,27 @@ Pagemap *newPagemap()
             uint64_t paddr = kbad_tag->physical_base_address + (vaddr - kbad_tag->virtual_base_address);
             uint64_t length = pmr.length;
 
-            for (uint64_t t = 0; t < length; t += 0x1000)
+            for (uint64_t t = 0; t < length; t += page_size)
             {
                 pagemap->mapMem(vaddr + t, paddr + t);
             }
         }
 
-        for (uint64_t i = 0x1000; i < 0x100000000; i += 0x1000)
+        for (uint64_t i = 0; i < 0x100000000; i += large_page_size)
         {
-            pagemap->mapMem(i, i);
-            pagemap->mapMem(i + hhdm_tag->addr, i);
+            pagemap->mapMem(i, i, Present | ReadWrite, true);
+            pagemap->mapMem(i + hhdm_tag->addr, i, Present | ReadWrite, true);
         }
 
         for (size_t i = 0; i < mmap_tag->entries; i++)
         {
             stivale2_mmap_entry &mmap = mmap_tag->memmap[i];
 
-            uint64_t base = ALIGN_DOWN(mmap.base, 0x1000);
-            uint64_t top = ALIGN_UP(mmap.base + mmap.length, 0x1000);
+            uint64_t base = ALIGN_DOWN(mmap.base, page_size);
+            uint64_t top = ALIGN_UP(mmap.base + mmap.length, page_size);
             if (top < 0x100000000) continue;
 
-            for (uint64_t t = base; t < top; t += 0x1000)
+            for (uint64_t t = base; t < top; t += page_size)
             {
                 if (t < 0x100000000) continue;
 
@@ -201,7 +203,9 @@ Pagemap *newPagemap()
             }
         }
 
-        pagemap->mapMem(framebuffer::frm_addr, framebuffer::frm_addr - hhdm_tag->addr, Present | ReadWrite | CacheDisable);
+        uint64_t frm_addr = ALIGN_DOWN(framebuffer::frm_addr, large_page_size);
+        uint64_t frm_size = ALIGN_UP(framebuffer::frm_height * framebuffer::frm_pitch, large_page_size);
+        pagemap->mapMemRange(frm_addr, frm_addr - hhdm_tag->addr, frm_size, Present | ReadWrite | CacheDisable, true);
 
         return pagemap;
     }
